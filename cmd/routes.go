b55286370f9423b11253dab88550a944b4c6a77e
hata-tomoyuki <h.tomo0602@gmail.com>
2026-03-28 09:35:42 +0900

    routes分離

---
+++ cmd/routes.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	repo "example.com/ecommerce/internal/adapters/postgresql/sqlc"
+	"example.com/ecommerce/internal/orders"
+	"example.com/ecommerce/internal/products"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	productService := products.NewService(repo.New(app.db))
+	productHandler := products.NewHandler(productService)
+	r.Get("/products", productHandler.ListProduct)
+	r.Get("/products/{id}", productHandler.FindProductById)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	orderService := orders.NewService(repo.New(app.db), app.db)
+	ordersHandler := orders.NewHandler(orderService)
+	r.Post("/orders", ordersHandler.PlaceOrder)
+
+	return r
+}
