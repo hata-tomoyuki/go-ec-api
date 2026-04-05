e59faac2e54e573a984b759ea27f59e67e62452f
hata-tomoyuki <h.tomo0602@gmail.com>
2026-04-05 20:32:37 +0900

    Graceful Shutdown

--- cmd/api.go
+++ cmd/api.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,9 +21,30 @@ func (app *application) run(h http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
-	slog.Info("starting server", "addr", app.config.addr)
+	// サーバーをゴルーチンで起動し、エラーをチャネルで受け取る
+	serverErr := make(chan error, 1)
+	go func() {
+		slog.Info("starting server", "addr", app.config.addr)
+		serverErr <- srv.ListenAndServe()
+	}()
 
-	return srv.ListenAndServe()
+	// OS シグナル（Ctrl+C / kill）を待ち受けるチャネル
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// シグナルまたはサーバーエラーを待ち受けて、グレースフルシャットダウンを実行する
+	select {
+	case err := <-serverErr:
+		return err
+	case <-quit:
+		slog.Info("shutting down server")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		defer cancel()
+
+		return srv.Shutdown(ctx)
+	}
 }
 
 type application struct {
